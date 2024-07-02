f4471d03803e5cd20435848aa1a6bad2a9b3dabe
Saikat Samanta <95569573+psykat1116@users.noreply.github.com>
2024-07-02 17:03:04 +0530

    Add Generate Functionality

---
+++ action/ingress.ts
@@ -0,0 +1,99 @@
+"use server";
+import {
+  IngressAudioEncodingPreset,
+  IngressInput,
+  IngressClient,
+  IngressVideoEncodingPreset,
+  RoomServiceClient,
+  TrackSource,
+  CreateIngressOptions,
+  IngressVideoOptions,
+  IngressAudioOptions,
+  IngressInfo,
+} from "livekit-server-sdk";
+
+import { db } from "@/lib/db";
+import { getSelf } from "@/lib/authService";
+import { revalidatePath } from "next/cache";
+
+const roomService = new RoomServiceClient(
+  process.env.LIVEKIT_API_URL!,
+  process.env.LIVEKIT_API_KEY!,
+  process.env.LIVEKIT_API_SECRET!
+);
+
+const ingressClient = new IngressClient(process.env.LIVEKIT_API_URL!);
+
+export const resetIngress = async (hostIdentity: string) => {
+  const ingresses = await ingressClient.listIngress({
+    roomName: hostIdentity,
+  });
+
+  const rooms = await roomService.listRooms([hostIdentity]);
+  for (const room of rooms) {
+    await roomService.deleteRoom(room.name);
+  }
+  for (const ingress of ingresses) {
+    if (ingress.ingressId) {
+      await ingressClient.deleteIngress(ingress.ingressId);
+    }
+  }
+};
+
+export const createIngress = async (ingressType: IngressInput) => {
+  try {
+    const self = await getSelf();
+    if (!self) {
+      throw new Error("User not found");
+    }
+
+    // *reset existing ingress*
+    await resetIngress(self.id);
+
+    const options: CreateIngressOptions = {
+      name: self.username,
+      roomName: self.id,
+      participantName: self.username,
+      participantIdentity: self.id,
+    };
+
+    if (ingressType === IngressInput.WHIP_INPUT) {
+      options.enableTranscoding = false;
+    } else {
+      options.video = new IngressVideoOptions({
+        source: TrackSource.CAMERA,
+        encodingOptions: {
+          value: IngressVideoEncodingPreset.H264_1080P_30FPS_3_LAYERS,
+          case: "preset",
+        },
+      });
+      options.audio = new IngressAudioOptions({
+        source: TrackSource.MICROPHONE,
+        encodingOptions: {
+          value: IngressAudioEncodingPreset.OPUS_STEREO_96KBPS,
+          case: "preset",
+        },
+      });
+    }
+
+    const ingress = await ingressClient.createIngress(ingressType, options);
+    if (!ingress || !ingress.url || !ingress.streamKey) {
+      throw new Error("Failed to create ingress");
+    }
+
+    await db.stream.update({
+      where: { userId: self.id },
+      data: {
+        ingressId: ingress.ingressId,
+        serverUrl: ingress.url,
+        streamKey: ingress.streamKey,
+      },
+    });
+
+    revalidatePath(`/u/${self.username}/keys`);
+    return ingress.toJson();
+  } catch (error) {
+    console.log("[CREATE_INGRESS_ERROE]", error);
+    return null;
+  }
+};
--- components/keys/ConnectModal.tsx
+++ components/keys/ConnectModal.tsx
@@ -1,5 +1,7 @@
 "use client";
-import React from "react";
+import React, { useTransition, useRef, ElementRef, useState } from "react";
+import { IngressInput } from "livekit-server-sdk";
+import { createIngress } from "@/action/ingress";
 
 import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert";
 import {
@@ -19,8 +21,29 @@ import {
   SelectTrigger,
   SelectValue,
 } from "@/components/ui/select";
+import { toast } from "sonner";
+
+const RTMP = String(IngressInput.RTMP_INPUT);
+const WHIP = String(IngressInput.WHIP_INPUT);
+
+type IngressType = typeof RTMP | typeof WHIP;
 
 const ConnectModal = () => {
+  const closeRef = useRef<ElementRef<"button">>(null);
+  const [isPending, startTransition] = useTransition();
+  const [ingressType, setIngressType] = useState<IngressType>(RTMP);
+
+  const onSubmit = async () => {
+    startTransition(() => {
+      createIngress(parseInt(ingressType))
+        .then(() => {
+          toast.success("Ingress created");
+          closeRef.current?.click();
+        })
+        .catch(() => toast.error("Failed to create Ingress"));
+    });
+  };
+
   return (
     <Dialog>
       <DialogTrigger asChild>
@@ -30,13 +53,17 @@ const ConnectModal = () => {
         <DialogHeader>
           <DialogTitle>Generate Connection</DialogTitle>
         </DialogHeader>
-        <Select>
+        <Select
+          disabled={isPending}
+          value={ingressType}
+          onValueChange={(value) => setIngressType(value)}
+        >
           <SelectTrigger className="w-full">
             <SelectValue placeholder="Ingress Type" />
           </SelectTrigger>
           <SelectContent>
-            <SelectItem value="RTMP">RTMP</SelectItem>
-            <SelectItem value="WHIP">WHIP</SelectItem>
+            <SelectItem value={RTMP}>RTMP</SelectItem>
+            <SelectItem value={WHIP}>WHIP</SelectItem>
           </SelectContent>
         </Select>
         <Alert>
@@ -48,10 +75,10 @@ const ConnectModal = () => {
           </AlertDescription>
         </Alert>
         <div className="flex justify-between">
-          <DialogClose>
+          <DialogClose ref={closeRef} asChild>
             <Button variant="ghost">Cancel</Button>
           </DialogClose>
-          <Button onClick={() => {}} variant="primary">
+          <Button disabled={isPending} onClick={onSubmit} variant="primary">
             Generate
           </Button>
         </div>
